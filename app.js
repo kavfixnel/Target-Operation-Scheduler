5afe88336213b5d2d6067f210b9320991b6022b1
Alexander Proschek <alexander.proschek@gmail.com>
2019-02-06 21:31:55 -0500

    Finished the alorithm to fulfill basic requirements

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const timeStamp1 = new Date();
+
 // Import statements
 const fs = require('fs');
 const dateTime = require('date-and-time');
 
-const schema = require('./schemas/csvSchema');
+const schema = require('./schemas/csvInputSchema.js');
 
 // Files to be processed
 const files = [
@@ -20,8 +22,8 @@ files.forEach(e => {
 
         // Split up the datapoints
         const elements = ee.split(',');
-        if(ee.length != schema.length) {
-            //console.error("Wrong schema length for given data!");
+        if(elements.length != schema.length) {
+            console.error("Wrong schema length for given data!");
         }
         for(i = 0; i < schema.length; i++) {
             // Preprocessing
@@ -33,4 +35,38 @@ files.forEach(e => {
         // Push dataPoint{} to data[]
         data.push(dataPoint);
     });
+});
+
+// Sort the data array based on 
+data.sort((e1, e2) => {
+    return e1.start_time - e2.start_time;
+});
+
+// Check if there are any overlapping time intervals
+for(i = 0; i < data.length - 1; i++) {
+    if(data[i].end_time > data[i+1].start_time) {
+        console.error("Overlapping time intervals at the "+i+"th and the "+(i+1)+"th index");
+    }
+}
+
+// Create the output string
+var outStr = "Event Time (UTC),Duration of Mode (s),Active mode\n";
+for(i = 0; i < data.length-1; i++) {
+    var line1 = data[i].start_time+","+data[i].duration+",acive mode\n";
+    var line2 = data[i].end_time+","+((data[i+1].start_time-data[i].end_time)/1000)+",cruise mode\n";
+    outStr += line1 + line2;
+}
+var line1 = data[data.length-1].start_time+","+data[data.length-1].duration+",acive mode\n";
+var line2 = data[data.length-1].end_time+",,cruise mode\n";
+outStr += line1 + line2;
+
+// Save to ouput.csv file
+fs.writeFile('output.csv', outStr, err => {
+    if(err) {
+        console.error("Error when writing the output file");
+    }
+
+    const timeStamp2 = new Date();
+    console.log("output.csv sucessfully created!");
+    console.log("Time Spent: "+((timeStamp2-timeStamp1)/1000)+"s");
 });
--- schemas/csvInputSchema.js
+++ schemas/csvInputSchema.js
@@ -1,4 +1,15 @@
-// Declaration of the schema of the CSV file
+/*
+  Schema must follow these rules
+  (1) Each attrbute must be in the format of
+      {
+          'name':'...',
+          'funciton': e => some preprocessing function
+      }
+  (2) There must be an attribute with name:'start_time'
+  (3) There must be an sttribute with name:'end_time
+*/
+
+// Declaration of the schema for the CSV input file(s)
 const schema = [
     {
         'name':'access',
